e1241b9926bb7875f720b0e05d9e1af7607e47fd
BaptisS <56864846+BaptisS@users.noreply.github.com>
2020-05-25 14:28:04 +0200

    Update natv2_storage_cloudinit.sh

--- natv2_storage_cloudinit.sh
+++ natv2_storage_cloudinit.sh
@@ -8,6 +8,16 @@ firewall-cmd --permanent --zone=public --add-port=443/tcp
 firewall-cmd --permanent --zone=public --add-masquerade 
 firewall-cmd --permanent --zone=public --add-forward-port=port=443:proto=tcp:toport=443:toaddr=1.2.3.4
 firewall-cmd --reload
-echo 1.2.3.4 > /home/opc/curip
+
+cd /home/opc/
+echo 1.2.3.4 > curip
+wget https://raw.githubusercontent.com/BaptisS/oci_nat_storage/master/netmask.sh
+wget https://raw.githubusercontent.com/BaptisS/oci_nat_storage/master/natcheck.sh
+chmod +x netmask.sh
+chmod +x natcheck.sh
+
+#echo "0 */12 * * * /home/opc/natcheck.sh" |crontab -
+echo "* * * * * /home/opc/natcheck.sh" |crontab -
+
 #firewall-cmd --permanent --zone=testing --add-rich-rule='rule family=ipv4 source address=192.168.1.0/24 masquerade'
 #firewall-cmd --permanent --zone=testing --add-rich-rule='rule family=ipv4 source address=192.168.1.0/24 forward-port port=22 protocol=tcp to-port=2222 to-addr=10.0.0.10'
