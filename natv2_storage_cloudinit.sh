50351108453c6a49a2a23d21d510b5c2a8591884
BaptisS <56864846+BaptisS@users.noreply.github.com>
2020-05-25 14:15:01 +0200

    Create natv2_storage_cloudinit.sh

---
+++ natv2_storage_cloudinit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo su
+echo net.ipv4.ip_forward = 1 >> /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf 
+systemctl mask iptables
+systemctl stop iptables
+firewall-cmd --permanent --zone=public --add-port=443/tcp 
+firewall-cmd --permanent --zone=public --add-masquerade 
+firewall-cmd --permanent --zone=public --add-forward-port=port=443:proto=tcp:toport=443:toaddr=1.2.3.4
+firewall-cmd --reload
+echo 1.2.3.4 > /home/opc/curip
+#firewall-cmd --permanent --zone=testing --add-rich-rule='rule family=ipv4 source address=192.168.1.0/24 masquerade'
+#firewall-cmd --permanent --zone=testing --add-rich-rule='rule family=ipv4 source address=192.168.1.0/24 forward-port port=22 protocol=tcp to-port=2222 to-addr=10.0.0.10'
